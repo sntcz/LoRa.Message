715d4438c217deecd30e5d2cbadf9beed095664d
Tomas Arci Kouba <tomas.kouba@gmail.com>
2019-10-17 12:04:35 +0200

    Option help texts to application

--- LoRaPacket/DecodePacketOptions.cs
+++ LoRaPacket/DecodePacketOptions.cs
@@ -5,22 +5,22 @@ using CommandLine;
 
 namespace LoRaPacket
 {
-    [Verb("decode")]
+    [Verb("decode", HelpText = "Decodes and prints message payload content")]
     class DecodePacketOptions
     {
-        [Option(SetName = "base64")]
+        [Option(SetName = "base64", HelpText = "Payload in BASE-64 format.")]
         public string Base64 { get; set; }
 
-        [Option(SetName = "hex")]
+        [Option(SetName = "hex", HelpText = "Payload in HEX format")]
         public string Hex { get; set; }
 
-        [Option]
+        [Option(HelpText = "Network session key in HEX format, (for data message decryption)")]
         public string NwkSKey { get; set; }
 
-        [Option]
+        [Option(HelpText = "Application session key in HEX format (for data message decryption)")]
         public string AppSKey { get; set; }
 
-        [Option]
+        [Option(HelpText = "Application key in HEX format (used for Join-Accept message)")]
         public string AppKey { get; set; }
 
     }
